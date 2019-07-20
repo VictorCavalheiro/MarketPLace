4b2e8d991bbefa4effe959180c1bf0e803e46e96
iago meijon <iagomeijonm@gmail.com>
2019-07-20 11:29:43 +0000

    criação do modelo de anuncio

---
+++ models/ad.js
@@ -0,0 +1,22 @@
+//framework for work with  mongo 
+var mongoose = require("mongoose");
+
+//setando a new db schema 
+var adSchema = new mongoose.Schema({
+    
+    name : String,
+    image :String,
+    description : String,
+    price : Number,
+    author : {
+            id:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref:"User"
+            },
+             username: String
+    }
+    
+});
+
+//creating the template "Ad" that uses the above schema, with this template make all op
+module.exports = mongoose.model("Ad", adSchema );
