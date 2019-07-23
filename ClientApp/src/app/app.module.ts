2339d9ab09cb70cb60f034dc0af95f56e9736aea
victor cavalheiro <victor.cavalheiro17@gmail.com>
2019-07-23 11:49:20 -0300

    register angular

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 //import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
@@ -12,7 +14,8 @@ import { FormularioComponent } from './formulario/formulario.component';
   ],
   imports: [
     BrowserModule,
-    
+    FormsModule,
+    HttpModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/formulario/formulario.component.ts
+++ ClientApp/src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-formulario',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+
+  user: any = {
+    name:'victor',
+    email:'victor.cavalheiro17@gmail.com',
+    region:'+701',
+    phoneNumber:'91234206',
+    work:'dev',
+    password:'1234',
+    repeatPassword:'1234'
+  }
+
+  onSubmit(form){
+    console.log(form.value);
+  }
 
   constructor() { }
 
